3490095bb08ef44ad84f6855dfa854d9edd0618b
Gump0 <blenderjack2004@gmail.com>
2024-02-10 18:19:27 -0500

    Reworking obstacles once more! Got inheritance to work correctly just that I static objects are struggling to recieve a reference despit all I've done so far. Gonna take a break and look at it inna bit, just pushing all the changes I've made the past couple hours :P

--- Assets/Scripts/Obstacles/Obstacles.cs
+++ Assets/Scripts/Obstacles/Obstacles.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//PRIMARY OBSTACLE CLASS//
 public class Obstacles : MonoBehaviour
 {
     public ObstacleSpawner obstaclespawner; // Instance of ObstacleSpawner
@@ -16,29 +17,51 @@ public class Obstacles : MonoBehaviour
     {
         obstaclespawner = spawner;
     }
-
+    // void Start()
+    // {
+    //     obstaclespawner = GetComponent<ObstacleSpawner>();
+    // }
     public void Update()
     {
-        MoveObstacle();
+        MoveObstacle(); // Constantly call and update MoveObstacle function
     }
 
-    private void MoveObstacle()
+    protected virtual void MoveObstacle()
     {
         if (obstaclespawner == null)
         {
             obstaclespawner = GetComponent<ObstacleSpawner>();
         }
 
-        if (obstaclespawner != null)
+        string obstTag = gameObject.tag;
+
+        if (obstTag == "StaticObstacle")
         {
-            transform.Translate(Vector2.left * obstaclespawner.obstacleSpeed * Time.deltaTime, Space.World);
+            // Use logic from the StaticObstacles subclass
+            StaticObstacles staticObstacle = GetComponent<StaticObstacles>();
+            if (staticObstacle != null)
+            {
+                staticObstacle.MoveObstacle();
+            }
+            else
+            {
+                Debug.LogWarning("StaticObstacles component not found on the object with the tag 'StaticObstacle'.");
+            }
         }
-        else
+        else if (obstTag == "DynamicObstacle")
         {
-            Debug.LogError("ObstacleSpawner component not found.");
+            // Use logic from the CarObstacles subclass
+            CarObstacles dynamicObstacle = GetComponent<CarObstacles>();
+            if (dynamicObstacle != null)
+            {
+                dynamicObstacle.MoveObstacle();
+            }
+            else
+            {
+                Debug.LogWarning("CarObstacles component not found on the object with the tag 'DynamicObstacle'.");
+            }
         }
     }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("GOBACKTOYOURCOUNTRY"))
@@ -57,4 +80,44 @@ public class Obstacles : MonoBehaviour
             SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
         }
     } 
+
+    // public static Obstacles CreateObstaclesWithTag(string tag)
+    // {
+    //     if(tag == "StaticObstacle")
+    //     {
+    //         return new StaticObstacles();
+    //     }
+    //     else if(tag == "DynamicObstacle")
+    //     {
+    //         return new CarObstacles();
+    //     }
+    //     else{
+    //         Debug.Log("Unkown obj tag: " +tag);
+    //         return null;
+    //     }
+    // }
+}
+//CLASS FOR STATIC OBSTACLE LOGIC//
+public class StaticObstacles : Obstacles
+{
+    protected override void MoveObstacle()
+    {
+        // base.MoveObstacle();
+        
+        // GameObject statObst = GameObject.FindGameObjectWithTag("StaticObstacle");
+        // //statObst.transform.position = transform.Translate(Vector2.left * obstaclespawner.obstacleSpeed * Time.deltaTime, Space.World);
+
+        // Debug.Log("STATICOBJECT IS BEING CALLED YAY!!!");
+    }
+}
+//CLASS FOR DYNAMIC OBSTACLE LOGIC//
+public class CarObstacles : Obstacles
+{
+    public PlayerMovement playerMovement;// Instance of player move script to reference player speed :3
+
+    protected override void MoveObstacle()
+    {
+    //     base.MoveObstacle();
+    //     GameObject dynObst = GameObject.FindGameObjectWithTag("DynamicObstacle");
+    }
 }
