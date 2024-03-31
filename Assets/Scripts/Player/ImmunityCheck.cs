06396f32dab1c01f35c1d85268089ddaa5d61750
Gump0 <blenderjack2004@gmail.com>
2024-03-31 16:32:55 -0400

    Implemented funny rainbowsgit add .! :DDD

--- Assets/Scripts/Player/ImmunityCheck.cs
+++ Assets/Scripts/Player/ImmunityCheck.cs
@@ -5,13 +5,31 @@ using UnityEngine;
 public class ImmunityCheck : MonoBehaviour
 {
     public bool playerIsImmune;
+    private SpriteRenderer sr;
+    private Color oGColor;
+    [SerializeField] private Color32[] colorArray;
+    private float lerpTimer, lerpDuration = 0.25f;
+    private int currentColorIndex;
+    private Color targetColor;
 
+    void Start(){
+        sr = GetComponent<SpriteRenderer>();
+        oGColor = sr.color;
+    }
     void Update(){
         if(playerIsImmune == true){
-            RainbowSheenOnImmunity(); 
+            RainbowSheenOnImmunity();
+        } else{
+            sr.color = oGColor;
         }
     }
     void RainbowSheenOnImmunity(){
-        
+        targetColor = colorArray[currentColorIndex];
+        sr.color = Color.Lerp(sr.color, targetColor, lerpTimer / lerpDuration);
+        lerpTimer += Time.deltaTime;
+        if(lerpTimer >= lerpDuration){
+            lerpTimer = 0f;
+            currentColorIndex = (currentColorIndex + 1) % colorArray.Length;
+        }
     }
 }
